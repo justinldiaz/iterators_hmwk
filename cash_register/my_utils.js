5074c3aff70ec4e413b9a19df175b395c741f17b
Justin <justindiaz91@gmail.com>
2015-05-07 09:13:10 -0700

    functions

--- cash_register/my_utils.js
+++ cash_register/my_utils.js
@@ -14,12 +14,42 @@
             cb(arr[i], i, arr);
         }
     }
-    // myUtils.myMap
-    // myUtils.myReduce
+
+    myUtils.myMap = function myMap(arr, cb){
+     var len = arr.length;
+     var newArr = [];
+     for(var i=0; i<len; i++){
+     newArr[i] = cb(arr[i], i, arr);
+    }
+    return newArr;
+    }
+
+    myUtils.myReduce = function myReduce(arr, cb, val){
+     var len = arr.length;
+     val = val || 0;
+     for(var i=0; i<len; i++){
+     val = cb(val, arr[i], i, arr)
+     }
+     return val;
+    }
+
+    myUtils.buildElement = function buildElement(tag, str){
+        return "<"+tag+">"+str+"</"+tag+">";
+    }
     
-    // myUtils.buildElement
-    
-    // myUtils.toDollarAmount;
-    // myUtils.toCurrencyString;
+    myUtils.toDollarAmount = function toDollarAmount(amt){
+        return (Math.round((amt - Math.floor(amt))*100))/100 + Math.floor(amt)
+        return (Math.round(amt*100))/100
+    }
+    myUtils.toCurrencyString = function toCurrencyString(amt){
+        if (amt > 1){
+            return "$" + Math.round(amt*100))/100
+        }else{
+            return "$0." + Math.round(amt*100))/100
+        }
+    }
     
 }.call(this))
+
+
+
